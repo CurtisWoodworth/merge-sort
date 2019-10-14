6272979909aa2f2066a433bb3dcc95e3b2feef2b
Curtis Woodworth <woodworthc10@gmail.com>
2019-10-14 09:45:24 -0400

    added psuedocode

--- quick_sort.py
+++ quick_sort.py
@@ -1,3 +1,21 @@
+def quick_sort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quick_sort(A, p, q - 1)
+        quick_sort(A, q + 1, r)
+
+
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, r-1):
+        if A[j] <= x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i + 1], A[r] = A[r], A[i + 1]
+    return i + 1
+
+
 class QuickSort:
 
     def __init__(self):
@@ -6,8 +24,8 @@ class QuickSort:
     def sort(self):
         pass
 
-    def quick_sort(self):
-        pass
 
-    def partition(self):
-        pass
+arr = [6,3,7,4,5]
+quick_sort(arr, 0, len(arr)-1)
+print(arr)
+
